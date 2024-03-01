1c64647b742526852a9521c2604ae4350346ecea
Alibaba <abdulmanafaliu414@gmail.com>
2024-03-01 07:56:43 +0000

    changes made

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="container copyright text-center mt-4">
+      <p>
+        &copy; <span>Copyright</span>{" "}
+        <strong className="px-1">Food site</strong>{" "}
+        <span>All Rights Reserved</span>
+      </p>
+      <div className="credits">
+        {/* All the links in the footer should remain intact. */}
+        {/* You can delete the links only if you've purchased the pro version. */}
+        {/* Licensing information: https://bootstrapmade.com/license/ */}
+        {/* Purchase the pro version with working PHP/AJAX contact form: [buy-url] */}
+        Designed by alibaba
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
