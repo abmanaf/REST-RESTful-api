116ca9e9f47d2d695db835f3f613e63f7efcd1d2
Alibaba <abdulmanafaliu414@gmail.com>
2024-03-02 15:44:06 +0000

    changes made

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -9,7 +9,6 @@ const Footer = () => {
         <span>All Rights Reserved</span>
       </p>
       <div className="credits">
-        {/* All the links in the footer should remain intact. */}
         {/* You can delete the links only if you've purchased the pro version. */}
         {/* Licensing information: https://bootstrapmade.com/license/ */}
         {/* Purchase the pro version with working PHP/AJAX contact form: [buy-url] */}
