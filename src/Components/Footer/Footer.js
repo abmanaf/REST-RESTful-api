f201238ba28003a46702cc6f29cd1cd2a42b61b2
Alibaba <abdulmanafaliu414@gmail.com>
2024-03-03 09:21:39 +0000

    minor changes

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -8,11 +8,7 @@ const Footer = () => {
         <strong className="px-1">Food site</strong>{" "}
         <span>All Rights Reserved</span>
       </p>
-      <div className="credits">
-        {/* Licensing information: https://bootstrapmade.com/license/ */}
-        {/* Purchase the pro version with working PHP/AJAX contact form: [buy-url] */}
-        Designed by alibaba
-      </div>
+      <div className="credits">Designed by alibaba</div>
     </div>
   );
 };
